ac5d1734f8677eb61e6629dd62f9c4a4a47fb43e
dima-leveret <dimazayac@gmail.com>
2023-01-31 08:20:26 +0100

    connect firebase realtime database

--- screens/UsersScreen.js
+++ screens/UsersScreen.js
@@ -1,10 +1,50 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 import { Colors } from "../constants/colors";
+// import { getUsers } from "../store/firebase";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const URL =
+  "https://rn-email-app-19b9a-default-rtdb.europe-west1.firebasedatabase.app/";
 
 export const UsersScreen = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(URL + "/users.json")
+      .then((res) => {
+        console.log(res.data);
+        const fetchedUsers = [];
+        for (const key in res.data) {
+          fetchedUsers.unshift({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        setUsers(fetchedUsers);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log("fetched user =>", users);
+
+    // const fechedUsers = getUsers();
+    // setUsers(fechedUsers);
+    // console.log("fetched users =>", fechedUsers);
+  }, []);
+
   return (
     <View style={styles.constiner}>
       <Text style={styles.text}> Users screen </Text>
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id}
+        renderItem={(itemData) => {
+          return <Text> {itemData.item.name} </Text>;
+        }}
+      />
     </View>
   );
 };
@@ -18,5 +58,11 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "white",
+  },
+  text2: {
+    color: "bleck",
+  },
+  users: {
+    flex: 1,
   },
 });
